f953dde629466dccc581b751567e47981cc3219c
oohanee <hanaanammaliyyaa@gmail.com>
2026-03-13 19:58:20 +0700

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,26 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://portfolio.vercel.app'
+  const baseUrl = 'https://folio-showcase.vercel.app'
 
   return [
     {
       url: `${baseUrl}/building-for-web`,
+      lastModified: new Date(),
+    },{
+      url: `${baseUrl}/portfolio`,
+      lastModified: new Date(),
+    },{
+      url: `${baseUrl}/lhi-access`,
+      lastModified: new Date(),
+    },{
+      url: `${baseUrl}/sik-lhi`,
+      lastModified: new Date(),
+    },{
+      url: `${baseUrl}/bitani-point`,
+      lastModified: new Date(),
+    },{
+      url: `${baseUrl}/bookshelf-api`,
       lastModified: new Date(),
     },
     {
